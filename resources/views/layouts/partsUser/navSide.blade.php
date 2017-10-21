1c0c90c4a0a434de381b505da130456512b7bbe1
Olivandro <aball615@gmail.com>
2017-10-21 10:39:16 +0100

    removed nav links that were not being used from side bar. Reshaped nav to have one ul for the user, and an additional ul for the admin controls. Also removed search bar option.

--- resources/views/layouts/partsUser/navSide.blade.php
+++ resources/views/layouts/partsUser/navSide.blade.php
@@ -1,34 +1,26 @@
 <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+
   <ul class="nav nav-pills flex-column">
     <li class="nav-item">
       <a class="nav-link active" href="/account">Account Overview <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="/account/deposit">Deposit</a>
-    </li>
-      @if (Auth::user()->admin)
-    <li class="nav-item">
-      <a class="nav-link" href="#">Trade</a>
+      <a class="nav-link" href="/account/deposit">Deposit Argument</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="/account/pending">Pending Deposits</a>
+      <a class="nav-link" href="/forum">Argument Landscape</a>
     </li>
-    @endif
   </ul>
 
-  <ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-      <a class="nav-link" href="/forum">Community Forum</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/forum/create">Create Post</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Messages</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Settings</a>
-    </li>
-  </ul>
+  @if (Auth::user()->admin)
+    <ul class="nav nav-pills flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="/account/pending">Pending Deposits</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/forum/create">Broadcast Notices</a>
+      </li>
+    </ul>
+  @endif
 
 </nav>
